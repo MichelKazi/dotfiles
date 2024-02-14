307c9e7d3903306c272f2c6362002af0c18706b3
Michel Kazi <michel.m.kazi@gmail.com>
2024-02-14 07:11:44 -0600

    lazy

---
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,20 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+local Util = require("lazyvim.util")
+
+local map = Util.safe_keymap_set
+
+map("n", "<esc><esc>", function()
+  -- vim.cmd("<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>")
+  require("notify").dismiss({ silent = true, pending = true })
+end, { desc = "Redraw / clear hlsearch / diff update" })
+
+map("n", "<C-n>", function()
+  require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+end, { desc = "Toggle tree (cwd)" })
+
+map("n", "<C-e>", function()
+  require("neo-tree.command").execute({ toggle = true, dir = Util.root() })
+end, { desc = "Toggle tree (cwd)" })
---
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,10 @@
+return {
+  "zbirenbaum/copilot.lua",
+  enabled = false,
+  cmd = "Copilot",
+  event = "InsertEnter",
+  opts = {
+    suggestion = { enabled = false },
+    panel = { enabled = false },
+  },
+}
